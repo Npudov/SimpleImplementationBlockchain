39195a10f0072af7e2d7281d6a9e7815963066e6
somagic2 <nik.p008@gmail.com>
2023-04-06 23:53:57 +0300

    Create Controller

--- src/main/kotlin/BlockJsonObject.kt
+++ src/main/kotlin/BlockJsonObject.kt
@@ -3,5 +3,6 @@ import com.google.gson.Gson
 
 object BlockJsonObject {
     var block: Block? = null
+    var lastActualIndex = 0L
     val gsonObj = Gson()
 }
---
+++ src/main/kotlin/controller/Controller.kt
@@ -0,0 +1,56 @@
+package controller
+
+import BlockJsonObject
+import chain.Block
+import chain.Node
+import processes.Connector.sendInfoToAllProc
+import processes.Proc
+import processes.isHighLevelProc
+
+object Controller {
+    const val INTERVAL = 50L
+
+
+    fun blockReceivedController(block: Block, node: Node): Boolean {
+        if (node.isCorrectBlock(block)) {
+            node.setLastBlock(block)
+            return true
+        }
+        return false
+    }
+
+    fun blockReceivedActualController(block: Block, node: Node): Boolean {
+        node.setLastBlock(block)
+        return true
+    }
+
+    fun stepBlockController(node: Node, proc: Proc): Boolean {
+        val stepBlock: Block? = node.attemptMakeCorrectBlock()
+        if (stepBlock != null) {
+            proc.sendInfoToAllProc(stepBlock)
+            return true
+        }
+        return false
+    }
+
+    fun blockActualToAllController(node: Node, proc: Proc): Boolean {
+        val blockActual = node.getLastBlock().copy(isActual=true)
+        BlockJsonObject.lastActualIndex = blockActual.index
+        proc.sendInfoToAllProc(blockActual)
+        return true
+    }
+
+    fun isBlockReceived(block: Block?): Boolean {
+        return block != null && !block.isActual
+    }
+
+    fun isBlockReceivedActual(block: Block?, proc: Proc): Boolean {
+        return block != null && !isHighLevelProc(proc)
+    }
+
+    fun isBlockBroadcastActual(node: Node, proc: Proc): Boolean {
+        return BlockJsonObject.lastActualIndex != node.getLastBlock().index
+                && node.getLastBlock().index % INTERVAL == 0L
+                && isHighLevelProc(proc)
+    }
+}
